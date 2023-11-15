2b9dec43bdb1c10c6b50bb883a2347cbab855727
Irvin <122047582+irvch@users.noreply.github.com>
2023-11-14 20:52:47 -0500

    changing to KL divergence

--- OT_ND_weighted.m
+++ OT_ND_weighted.m
@@ -5,7 +5,7 @@ rng('default');
 
 % STARTING PARAMETERS
 eta = 0.1;          % INITIAL STEP SIZE
-lam = 6e7;          % REGULARIZATION PARAMETER (HIGHER = BETTER ALIGNMENT BUT MORE ITERATIONS)
+lam = 5e7;          % REGULARIZATION PARAMETER
 
 %time_hist = zeros(40,1);
 %iter_hist = zeros(40,1);
@@ -19,53 +19,110 @@ lam = 6e7;          % REGULARIZATION PARAMETER (HIGHER = BETTER ALIGNMENT BUT MO
 %y = table2array(readtable('revised data edit.xlsx', Sheet='slope (2)'));
 
 % PYRAMID DATA SETS
-%y = table2array(readtable('revised data set 1.xlsx', Sheet='every'));
-%x = table2array(readtable('revised data edit.xlsx', Sheet='every (3)'));
+y = table2array(readtable('revised data 3.xlsx', Sheet='every'));
+x = table2array(readtable('revised data set 1.xlsx', Sheet='every (3)'));
 
 % PYRAMID DATA WITH SHIFTED SOURCE
-x = table2array(readtable('revised data 3.xlsx', Sheet='every (2)'));
-y = table2array(readtable('revised data 3.xlsx', Sheet='every'));
+%x = table2array(readtable('revised data 3.xlsx', Sheet='every (3)'));
+%y = table2array(readtable('revised data set 1.xlsx', Sheet='every'));
 
 % 2D DATA POINTS
-%a = linspace(0,4,5);
-%b = linspace(0,4,5);
-%[A, B] = meshgrid(a, b);
+a = linspace(0,4,20);
+b = linspace(0,4,20);
+[A, B] = meshgrid(a, b);
 %x = [A(:) B(:)];
-%y = normrnd(7, 0.5, [25,2]);
+mu_x = [0 9];
+sigma_x = [0.25 0; 0 1];
+%x = mvnrnd(mu_x, sigma_x, 200);
 
-% PRECONDITIONING
-%x1 = x_old.*std(y)./std(x_old);
-%x = x1 - mean(x1) + mean(y);
+mu_y1 = [7 7];
+sigma_y1 = [1 0; 0 0.25];
+%y = mvnrnd(mu_y, sigma_y, 200);
+mu_y2 = [7 11];
+sigma_y2 = [0.1 0; 0 0.1];
+sigma_y3 = [10 0; 0 10];
 
-% START TIMER FOR ALGORITHM
-tic
+y1 = mvnrnd(mu_y1, sigma_y1, 200);
+y2 = mvnrnd(mu_y2, sigma_y2, 10);
+y3 = mvnrnd(mu_y1, sigma_y3, 30);
+%y = [y1; y2; y3];
 
-% RUNNING GRADIENT DESCENT
-[T_hist, L1_hist, L2_hist, L_hist, eta_hist, iter, min_index] = grad_descent(x, y, eta, lam);
-iters = 1:iter;
-fprintf("\nTotal iters: %d\n", iter)
-fprintf("Minimum achieved at iter: %d\n", min_index)
-fprintf("Final cost: %d\n\n", L2_hist(min_index,:))
+% MATCH THE DIMENSIONS
+[n, d_x] = size(x);
+[m, d_y] = size(y);
+dim_diff = abs(d_x - d_y);
 
-% MAP RUNTIME
-runtime = toc;
-
-% START TIMER FOR PLOTTING
-tic
+if d_x > d_y
+    newrow_y = zeros(m, dim_diff);
+    y = [y, newrow_y];
+elseif d_x < d_y
+    newrow_x = zeros(n, dim_diff);
+    x = [x, newrow_x];
+end
 
 % PLOTTING INITIAL DISTRIBUTIONS
 figure()
 hold on
-scatter3(x(:,1), x(:,2), x(:,3), 'filled', 'blue')
-title('SOURCE')
+if d_x == 2 && d_y == 2
+    scatter(y(:,1), y(:,2), 'filled', 'red')
+    scatter(x(:,1), x(:,2), 'filled', 'blue')
+elseif d_x == 3 || d_y == 3
+    scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red')
+    scatter3(x(:,1), x(:,2), x(:,3), 'filled', 'blue')
+end
+title('INITIAL DISTRIBUTIONS')
+legend('SOURCE', 'TARGET')
 hold off
 
-% PLOTTING DISTRIBUTIONS AFTER PRECONDITIONING
+% PRECONDITIONING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+s_x = sqrt(sum(abs(x-median(x)))/length(x)); % IMMUNE TO LARGE CLUSTERS OF OUTLIERS
+s_y = sqrt(sum(abs(y-median(y)))/length(y)); % ROBUST DISPERSION MEASURE USING L1 NORM
+
+%x1 = x.*std(y)./std(x);    % OG RESCALING BASED ON STD - BETTER FOR SMALL OUTLIERS
+x1 = x.*s_y./s_x;          % RESCALING X1 BASED ON ROBUST MEASURE
+%x1 = x.*s_y./std(x);           % USE STD OF X TO REACH MORE POINTS IN X
+
+% SHIFTING TO MEAN OR MEDIAN OF TARGET
+%x = x1 - mean(x1) + mean(y);
+x = x1 - median(x1) + median(y);
+
+% ADDING ANOTHER DIMENSION
+if d_x > d_y
+    y = [y(:,1), y(:,2), ones(m, dim_diff)*mean(y(:,3))];
+elseif d_x < d_y
+    x = [x(:,1), x(:,2), ones(n, dim_diff)*mean(y(:,3))];
+end
+
+
+% START TIMER FOR ALGORITHM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tic
+
+% RUNNING GRADIENT DESCENT
+[T_hist, L1_hist, L2_hist, L_hist, eta_hist, H_hist, iter, min_index] = grad_descent(x, y, eta, lam);
+iters = 1:iter;
+fprintf("\nTotal iters: %d\n", iter)
+fprintf("Minimum achieved at iter: %d\n", min_index)
+fprintf("Final cost: %d\n\n", L_hist(min_index,:))
+
+% MAP RUNTIME
+runtime = toc;
+
+% PLOTTING INITIAL DISTRIBUTIONS
 figure()
 hold on
-scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red')
-title('TARGET')
+if d_x == 2 && d_y == 2
+    scatter(y(:,1), y(:,2), 'filled', 'red')
+    scatter(x(:,1), x(:,2), 'filled', 'blue')
+elseif d_x == 3 || d_y == 3
+    scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red')
+    scatter3(x(:,1), x(:,2), x(:,3), 'filled', 'blue')
+end
+title('PRECONDITIONED DISTRIBUTIONS')
+legend('SOURCE', 'TARGET')
 hold off
+
+% START TIMER FOR PLOTTING
+tic
 
 figure()
 hold on
@@ -90,75 +147,21 @@ plot(iters, eta_hist, '-')
 title('ETA')
 hold off
 
-%{
-% PLOT 25 MAP LOCATION HISTORIES
-figure()
-hold on
-for i = 1:min_index
-    if mod(i, floor(min_index/25)) == 0
-        T_map = T_hist(:,:,i);
-        subplot(5, 5, i)
-        hold on
-        scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red')
-        scatter3(T_map(:,1), T_map(:,2), T_map(:,3), 'filled', 'green')
-        iterations = sprintf('ITERS: %d', i);
-        title(iterations)
-        hold off
-        if count < 25
-            count = count + 1;
-        end
-    end
-end
-hold off
-%}
-
-%{
-% PLOT TRAJECTORY OF EACH POINT
-figure()
-hold on
-for i = 1:iter
-    T_map_i1 = T_hist(:,:,i);
-    T_map_i2 = T_hist(:,:,i+1);
-    for j = 1:length(x)
-        plot([T_map_i1(j,1) T_map_i2(j,1)], [T_map_i1(j,2) T_map_i2(j,2)], color = 'green')
-    end
-end
-%}
-
 % PLOTTING FINAL OPTIMAL MAP
 figure()
 hold on
 T_map = T_hist(:,:,min_index);
-%scatter(x(:,1), x(:,2), 'filled', 'blue')
-scatter(y(:,1), y(:,2), 'filled', 'red')
-scatter(T_map(:,1), T_map(:,2), 'filled', 'green')
+if d_x == 2 && d_y == 2
+    %scatter(x(:,1), x(:,2), 'filled', 'blue')
+    p_y = scatter(y(:,1), y(:,2), 'filled', 'red');
+    p_t = scatter(T_map(:,1), T_map(:,2), 'filled', 'green');
+elseif d_x == 3 || d_y == 3
+    p_y = scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red');
+    p_t = scatter3(T_map(:,1), T_map(:,2),  T_map(:,3), 'filled', 'green');
+end
 title('FINAL MAP')
+legend([p_y, p_t], {'TARGET', 'MAP'})
 hold off
-
-% NEAREST NEIGHBOR SEARCH
-%idk = knnsearch(T_map, y);
-%new = T_map(idk,:);
-
-% PLOTTING NEAREST TARGET POINTS TO OPTIMAL MAP RESULTS
-%figure()
-%hold on
-%scatter3(new(:,1), new(:,2), new(:,3), 'filled', 'green')
-%scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red');
-%title('NEAREST NEIGHBORS RESULT')
-%hold off
-
-% COMPARING TARGET SET BEFORE AND AFTER NEAREST NEIGHBORS SEARCH
-%figure()
-%hold on
-%subplot(1, 2, 1);
-%scatter3(y(:,1), y(:,2), y(:,3), 'filled', 'red')
-%title('INITIAL Y');
-
-%subplot(1, 2, 2);
-%scatter3(new(:,1), new(:,2), new(:,3), 'filled', 'green');
-%title('FINAL');
-%hold off
-
 
 % END TIMER AND DISPLAY RUNTIME
 plotting = toc;
@@ -167,15 +170,24 @@ disp(['PLOT RUNTIME: ' num2str(plotting) ' sec'])
 disp(['TOTAL RUNTIME: ' num2str(runtime + plotting) ' sec'])
 
 
-% BEGINNING OPTIMAL TRANSPORT ALGORITHM
+% BEGINNING OPTIMAL TRANSPORT ALGORITHM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % BANDWIDTH MATRIX SELECTION WITH SILVERMAN'S RULE OF THUMB
-function H = bandwidth(x, n)
-    [~, d] = size(x);                               
-    if d == 1 % FOR ONE-DIMENSIONAL CASE
-        H = 0.9*min(std(x), iqr(x)/1.34)*n^(-1/5);
-    else      % FOR MULTIDIMENSIONAL CASE
-        H = std(x)*(4/((d+2)*n))^(1/(d+4));
-    end
+function H = bandwidth(x, n, std_switch)
+    [~, d] = size(x);
+    if std_switch == false
+        if d == 1 % FOR ONE-DIMENSIONAL CASE
+            H = 0.9*min(std(x), iqr(x)/1.34)*n^(-1/5);
+        else
+            H = std(x)*(4/((d+2)*n))^(1/(d+4));
+        end
+    else
+        s_x = sqrt(sum(abs(x-median(x)))/length(x)); % IMMUNE TO LARGE CLUSTERS OF OUTLIERS
+        if d == 1 % FOR ONE-DIMENSIONAL CASE
+            H = 0.9*min(s_x, iqr(x)/1.34)*n^(-1/5);
+        else
+            H = s_x*(4/((d+2)*n))^(1/(d+4));
+        end
+    end    
 end
 
 % COST FUNCTION C (RETURNS CONSTANT)
@@ -191,63 +203,57 @@ end
 % WEIGHT FUNCTION USING MULTIDIMENSIONAL GAUSSIAN DENSITY
 function weights = Weights(x1, x2, Hx)
     [n, d] = size(x1);
-    func1 = zeros(d,n,n);
+    p = zeros(d,n,n);
     for i = 1:d
-        func1(i,:,:) = (x1(:,i)' - x2(:,i))./Hx(:,i);
+        p(i,:,:) = (x1(:,i)' - x2(:,i))./Hx(:,i);
     end
-    f1 = sum(exp(-1/2.*sum(func1.^2, 1)), 3);
-    const1 = mean(diag(inv(diag(n^2 * (Hx*sqrt(2*pi)).^d))));
+    f1 = sum(exp(-1/2.*sum(p.^2, 1)), 3);
+    const1 =  1/(n^2 * (mean(Hx)*sqrt(2*pi)).^d);
     weights = const1.*(f1)';
 end
 
-% TEST FUNCTION DEFINING DISTANCE BETWEEN MAP AND TARGET (RETURNS CONSTANT)
-function test = F(Tx1, y, Tx2, Hx, Hy, weights)
+% TEST FUNCTION DEFINING KL DIVERGENCE MAP AND TARGET (RETURNS CONSTANT)
+function test = KL(Tx1, y, Tx2, Hx, Hy, weights)
     [n, d] = size(Tx1);
     m = length(y);
-
+    
     % INITIALIZE EMPTY MATRICES
-    func1 = zeros(d,n,n);
-    func2 = zeros(d,n,m);
-    func3 = zeros(d,m,n);
-    func4 = zeros(d,m,m);
+    p = zeros(d,n,n);
+    q = zeros(d,n,m);
 
     % FILL IN EACH MATRIX WITH THEIR RESPECTIVE VALUES AT EACH D
     for i = 1:d
-        % PART F1 WHERE CENTER POINTS ARE IN Tx
-        func1(i,:,:) = (Tx1(:,i)' - Tx2(:,i))./Hx(:,i);
-        func2(i,:,:) = (y(:,i)' - Tx2(:,i))./Hy(:,i);
-
-        % PART F2 WHERE CENTER POINTS ARE IN Y
-        func3(i,:,:) = (Tx1(:,i)' - y(:,i))./Hx(:,i);
-        func4(i,:,:) = (y(:,i)' - y(:,i))./Hy(:,i);
+        % DISTRIBUTION P(x) WITH CENTER POINTS IN Tx
+        p(i,:,:) = (Tx1(:,i)' - Tx2(:,i))./Hx(:,i);
+        
+        % DISTRIBUTION Q(x) WITH CENTER POINTS IN Tx
+        q(i,:,:) = (y(:,i)' - Tx2(:,i))./Hy(:,i);
     end
 
     % USE TWO SUM FUNCTIONS IN PLACE OF DOUBLE FOR-LOOP
-    f1 = sum(weights.*exp(-1/2.*sum(func1.^2, 1)), 'all');
-    f2 = sum(weights.*exp(-1/2.*sum(func2.^2, 1)), 'all');
-    f3 = sum(weights.*exp(-1/2.*sum(func3.^2, 1)), 'all');
-    f4 = sum(weights.*exp(-1/2.*sum(func4.^2, 1)), 'all');
+    p1 = sum(weights.' .* exp(-1/2.*sum(p.^2, 1)), 3);
+    q1 = sum(weights.' .* exp(-1/2.*sum(q.^2, 1)), 3);
 
     weight_sum = sum(weights);
-    
-    % CONSTANTS IN FRONT OF SUM
-    const1 = mean(diag(inv(diag(n^2 * (Hx*sqrt(2*pi)).^d * weight_sum))));
-    const2 = mean(diag(inv(diag(m*n * (Hy*sqrt(2*pi)).^d * weight_sum))));
-    const3 = mean(diag(inv(diag(n*m * (Hx*sqrt(2*pi)).^d * weight_sum))));
-    const4 = mean(diag(inv(diag(m^2 * (Hy*sqrt(2*pi)).^d * weight_sum))));
 
-%    const1 = 1/(n^2 * (Hx*sqrt(2*pi))^d * weight_sum);
-%    const2 = 1/(m*n * (Hy*sqrt(2*pi))^d * weight_sum);
-%    const3 = 1/(n*m * (Hx*sqrt(2*pi))^d * weight_sum);
-%    const4 = 1/(m^2 * (Hy*sqrt(2*pi))^d * weight_sum);
+    % CONSTANTS TO NORMALIZE AND FINISH COMPUTATION
+    const1 = 1/(n^2 * (mean(Hx)*sqrt(2*pi)).^d * weight_sum);
+    const2 = 1/(m*n * (mean(Hy)*sqrt(2*pi)).^d * weight_sum);
     
-    % FINAL RESULT MULTIPLYING CONSTANTS
-    test = const1.*(f1)' - const2.*(f2)' - const3.*(f3)' + const4.*(f4)';
+    % FINAL DISTRIBUTIONS
+    p_dist = const1 .* p1;
+    q_dist = const2 .* q1;
+
+    % ENTROPY DEFINING POINT-BY-POINT DIFFERENCE
+    entropy = log(p_dist) - log(q_dist);
+
+    % FINAL RESULT SUMMING TOTAL ENTROPY AND NORMALIZING
+    test = sum(entropy)/n;
 end
 
 % GLOBAL COST FUNCTION L (RETURNS CONSTANT)
 function l = L(x, y, Tx, Hx, Hy, lam, weights)
-    l = C(x, Tx, weights) + lam*(F(Tx, y, Tx, Hx, Hy, weights));
+    l = C(x, Tx, weights) + lam*(KL(Tx, y, Tx, Hx, Hy, weights));
 end
 
 % GRADIENT OF COST (RETURNS 1xD VECTOR)
@@ -265,80 +271,84 @@ function gradC = C_grad(x, Tx, weights)
 end
 
 % GRADIENT OF TEST FUNCTION TAKEN WITH RESPECT TO Tx CENTER
-function gradF = F_grad(Tx1, y, Tx2, Hx, Hy, weights)
+function gradF = KL_grad(Tx1, y, Tx2, Hx, Hy, weights)
     [n, d] = size(Tx1);
     m = length(y);
 
     % INITIALIZE EMPTY MATRICES
-    func1 = zeros(d,n,n);
-    func2 = zeros(d,n,m);
+    p = zeros(d,n,n);
+    q = zeros(d,n,m);
 
     % FILL IN EACH MATRIX WITH THEIR RESPECTIVE VALUES AT EACH D
     for i = 1:d
-        func1(i,:,:) = (Tx1(:,i)' - Tx2(:,i))./Hx(:,i);
-        func2(i,:,:) = (y(:,i)' - Tx2(:,i))./Hy(:,i);
+        % DISTRIBUTION P(x) WITH CENTER POINTS IN Tx
+        p(i,:,:) = (Tx1(:,i)' - Tx2(:,i))./Hx(:,i);
+
+        % DISTRIBUTION Q(x) WITH CENTER POINTS IN Tx
+        q(i,:,:) = (y(:,i)' - Tx2(:,i))./Hy(:,i);
+
+        for j = 1:d
+            p(i,j,j) = 0;
+            q(i,j,j) = 0;
+        end
     end
 
-    % USE TWO SUM FUNCTIONS IN PLACE OF DOUBLE FOR-LOOP
-    f1 = sum(func1.*exp(-1/2.*sum(func1.^2, 1)), 3);
-    f2 = sum(func2.*exp(-1/2.*sum(func2.^2, 1)), 3);
+    % SUMMATION TO CALCULATE ORIGINAL DISTRIBUTION
+    p1 = sum(weights.' .* exp(-1/2.*sum(p.^2, 1)), 'all');
+    q1 = sum(weights.' .* exp(-1/2.*sum(q.^2, 1)), 'all');
+
+    % USE TWO SUM FUNCTIONS IN PLACE OF DOUBLE FOR-LOOP FOR GRADIENT
+    grad_p1 = sum(weights.' .* p.*exp(-1/2.*sum(p.^2, 1)), 3);
+    grad_q1 = sum(weights.' .* q.*exp(-1/2.*sum(q.^2, 1)), 3);
 
     weight_sum = sum(weights);
 
-    % CONSTANTS IN FRONT OF SUM
-    c1 = inv(diag(n^2 * (Hx*sqrt(2*pi)).^d * Hx' * weight_sum));
-    c2 = inv(diag(m*n * (Hy*sqrt(2*pi)).^d * Hy' * weight_sum));
+    % CONSTANTS IN FRONT OF ORIGINAL DISTRIBUTION SUM
+    const1 = 1/(n^2 * (mean(Hx)*sqrt(2*pi)).^d * weight_sum);
+    const2 = 1/(m*n * (mean(Hy)*sqrt(2*pi)).^d * weight_sum);
 
-    %c1 = 1/(n^2 * (Hx*sqrt(2*pi))^d * Hx * weight_sum);
-    %c2 = 1/(m*n * (Hy*sqrt(2*pi))^d * Hy * weight_sum);
+    % CONSTANTS IN FRONT OF GRADIENT SUM
+    grad_p_const = 1/(n^2 * (mean(Hx)*sqrt(2*pi)).^d * mean(Hx) * weight_sum);
+    grad_q_const = 1/(m*n * (mean(Hy)*sqrt(2*pi)).^d * mean(Hy) * weight_sum);
     
     % FINAL GRADIENT VALUE
-    gradF = weights.*(c1.*(f1)' - c2.*(f2)');
+    gradF = (grad_p_const.*(grad_p1)')./(n*const1.*p1) - (grad_q_const.*(grad_q1)')./(n*const2.*q1);
 end
 
 % GRADIENT OF L (RETURNS NxD MATRIX)
 function gradL = L_grad(x, y, Tx, Hx, Hy, lam, weights)
-    gradL = C_grad(x, Tx, weights) + lam * F_grad(Tx, y, Tx, Hx, Hy, weights);
+    gradL = C_grad(x, Tx, weights) + lam * KL_grad(Tx, y, Tx, Hx, Hy, weights);
 end
 
 % ADAPTIVE LEARNING RATE ETA (RETURNS CONSTANT AND GRAD DESCENT RESULT)
 function [eta, Tx_next] = adapt_learning(x, y, Tx_curr, Hx, Hy, lam, eta, weights)
     eta = eta * 2;                                                                 % INCREASE ETA FOR FASTER CONVERGENCE
-    Tx_next = Tx_curr - (eta .* L_grad(x, y, Tx_curr, Hx, Hy, lam, weights));      % COMPUTE NEW MAP TX
+    l_grad = L_grad(x, y, Tx_curr, Hx, Hy, lam, weights);
+    Tx_next = Tx_curr - (eta .* l_grad);                                           % COMPUTE NEW MAP TX
     L_curr = L(x, y, Tx_curr, Hx, Hy, lam, weights);                               % COMPUTE COST BASED ON PAST MAP
     L_next = L(x, y, Tx_next, Hx, Hy, lam, weights);                               % COMPUTE COST BASED ON NEW MAP
-    max_while = 5;
     while L_curr < L_next % NEXT COST L SHOULD NOT BE GREATER THAN THE CURRENT ONE
-        if max_while < 0  % MAX LENGTH OF LOOP SHOULD NOT EXCEED 5 OR ETA WILL BECOME TOO SMALL
-            break
-        end
         eta = eta / 2;                                                             % SHRINK STEP SIZE
-        Tx_next = Tx_curr - (eta .* L_grad(x, y, Tx_curr, Hx, Hy, lam, weights));  % COMPUTE NEW MAP TX WITH NEW ETA
+        Tx_next = Tx_curr - (eta .* l_grad);                                       % COMPUTE NEW MAP TX WITH NEW ETA
         L_next = L(x, y, Tx_next, Hx, Hy, lam, weights);                           % COMPUTE COST BASED ON NEW MAP
-        max_while = max_while - 1;                                                 % KEEP TRACK OF WHILE-LOOP LENGTH
     end
 end
 
-% LINEARLY INCREASING LAMBDA AND DECREASING BANDWIDTH (RETURNS DxD MATRIX)
-%function [Hz_new, lam_new] = linear_change(Hy, Hz, i, it, H_const, lam_init, lam_final)
-%    Hz_new = (H_const * Hz * (it - i) / it) + (Hy * i / it);
-%    lam_new = (lam_init * (it - i) / it) + (lam_final * i / it); 
-%end
-
 % GRADIENT DESCENT
-function [T_hist, L1_hist, L2_hist, L_hist, eta_hist, iter, min_index] = grad_descent(x, y, eta, lam)
+function [T_hist, L1_hist, L2_hist, L_hist, eta_hist, H_hist, iter, min_index] = grad_descent(x, y, eta, lam)
     % INITIAL VALUES
-    Tx = x;                             % INITIAL MAP SHOULD BE THE ORIGINAL SET OF POINTS
-    z = [Tx; y];                        % COMBINED SET OF POINTS - FOR BANDWIDTH
-    Hy = bandwidth(y, length(x));       % BANDWIDTH FOR Y
-    Hz = bandwidth(z, length(x));       % BANDWIDTH FOR ALL POINTS
+    Tx = x;                                   % INITIAL MAP SHOULD BE THE ORIGINAL SET OF POINTS
+    z = [Tx; y];                              % COMBINED SET OF POINTS - FOR BANDWIDTH
+    Hy = bandwidth(y, length(x), true);       % BANDWIDTH FOR Y
+    Hz = bandwidth(z, length(x), true);       % BANDWIDTH FOR ALL POINTS
 
     % INITIALIZING EMPTY HISTORY FOR ALL PLOTS OVER TIME
     T_hist = Tx; % FIRST ENTRY IN MAP HISTORY SHOULD BE THE SOURCE DISTRIBUTION
+    H_hist = Hz;
     eta_hist = eta;
     weights = Weights(Tx, Tx, Hz);
     L1_hist = C(x, Tx, weights);
-    L2_hist = F(Tx, y, Tx, Hz, Hy, weights);
+    L2_hist = KL(Tx, y, Tx, Hz, Hy, weights);
     L_hist = L1_hist + lam * L2_hist;
 
     criteria = 1e8; % ARBITRARY LARGE NUMBER TO START
@@ -349,7 +359,7 @@ function [T_hist, L1_hist, L2_hist, L_hist, eta_hist, iter, min_index] = grad_de
     % CONTINUE UNTIL REACHING STOPPING CRITERIA
     while criteria > 0
         % FOR KEEPING TRACK OF ITERATION PROGRESS
-        if mod(iter, 20) == 0
+        if mod(iter, 100) == 0
             fprintf("Iteration: %d\n", iter)
         end
 
@@ -358,30 +368,30 @@ function [T_hist, L1_hist, L2_hist, L_hist, eta_hist, iter, min_index] = grad_de
         end
         iter = iter+1;
 
-        % GETTING NEW BANDWIDTH (DECREASE TO HY)
+        % GETTING NEW BANDWIDTH (CONVERGE TO HY)
         z = [Tx; y];
-        Hz = bandwidth(z, length(x));
+        Hz = bandwidth(z, length(x), true);
         Hz = (Hz + Hy) / 2;
-
-        % WEIGHTS OF EACH PIXEL BASED ON POSITION IN DENSITY PLOT
-        weights = Weights(Tx, Tx, Hz);
 
         % GET NEW MAP TX AND LEARNING RATE ETA AT EACH STEP
         [eta, Tx] = adapt_learning(x, y, Tx, Hz, Hz, lam, eta, weights);
-        criteria = F(Tx, y, Tx, Hz, Hy, weights);
 
         % ADD CURRENT VALUES TO HISTORY DATA FOR PLOTTING
         T_hist(:,:,iter) = Tx;
+        H_hist(:,:,iter) = Hz;
         eta_hist(iter,:) = eta;
+        weights = Weights(Tx, Tx, Hz);
         L1_hist(iter,:) = C(x, Tx, weights);
-        L2_hist(iter,:) = criteria;
+        L2_hist(iter,:) = KL(Tx, y, Tx, Hz, Hy, weights);
         L_hist(iter,:) = L1_hist(iter,:) + lam*(L2_hist(iter,:));
 
+        % DEFINING CRITERIA TO CONTINUE OR STOP ITERATION
+        criteria = L_hist(iter,:);
         if abs(criteria) < abs(minimum)
             minimum = criteria;
             min_index = iter;
-        %else
-        %    break
+        else
+            %break
         end
     end
 end
